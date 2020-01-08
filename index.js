877ebaa97cf9666e6d76d0d4d1ea0910a6bd8872
Sergio Rosa <scarlosjazz@gmail.com>
2020-01-08 18:52:18 -0500

    Primitives

---
+++ index.js
@@ -0,0 +1,16 @@
+// let name = "Sergio";
+// let age = 30;
+// let isApproved = false;
+// let firstName = undefined;
+// let selectedColor = null;
+
+// object literal
+let person = {
+  name: "Sergio",
+  age: 30
+};
+
+//Dot Notation
+person.name = "Mike";
+
+console.log(person.name);
